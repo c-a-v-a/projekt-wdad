d36d3d907f57d8c8da856c6714f0e8af0ac218a7
c-a-v-a <cavaevilmail@gmail.com>
2024-12-19 16:36:09 +0100

    add tex

--- main.r
+++ main.r
@@ -6,31 +6,46 @@ earnings.sd = 15000;
 earnings.mean = mean(earnings);
 earnings.se = earnings.sd / sqrt(length(earnings));
 
-# CONFIDENCE INTERVAL 0.9
+cat("Earnings mean: ", earnings.mean, "\n");
+cat("Earnings standard derivation: ", earnings.sd, "\n");
+cat("Earnings standard error: ", earnings.se, "\n");
+
 earnings.interval1 = 0.1;
 earnings.ci1.margin = qnorm(p = (1 - earnings.interval1 / 2)) * earnings.se;
 earnings.ci1 = earnings.mean + c(-1,1) * earnings.ci1.margin;
 
-# CONFIDENCE INTERVAL 0.95
+print("Confidence interval for 0.9 assuming normal distribution:");
+print(earnings.ci1);
+
 earnings.interval2 = 0.05;
 earnings.ci2.margin = qnorm(p = (1 - earnings.interval2 / 2)) * earnings.se;
 earnings.ci2 = earnings.mean + c(-1,1) * earnings.ci2.margin;
+
+print("Confidence interval for 0.95 assuming normal distribution:");
+print(earnings.ci2);
 
 # B
 earnings.sd = sd(earnings);
 earnings.se = earnings.sd / sqrt(length(earnings));
 
-# CONFIDENCE INTERVAL 0.9
+cat("Earnings standard derivation: ", earnings.sd, "\n");
+cat("Earnings standard error: ", earnings.se, "\n");
+
 earnings.interval1 = 0.1;
 earnings.ci1.margin = qt(p = (1 - earnings.interval1 / 2),
                          df = length(earnings) - 1) * earnings.se;
 earnings.ci1 = earnings.mean + c(-1,1) * earnings.ci1.margin;
 
-# CONFIDENCE INTERVAL 0.95
+print("Confidence interval for 0.9 assuming Student's t-distribution:");
+print(earnings.ci1);
+
 earnings.interval2 = 0.05;
 earnings.ci2.margin = qt(p = (1 - earnings.interval2 / 2),
                          df = length(earnings) - 1) * earnings.se;
 earnings.ci2 = earnings.mean + c(-1,1) * earnings.ci2.margin;
+
+print("Confidence interval for 0.95 assuming Student's t-distribution:");
+print(earnings.ci2);
 
 # C
 my_bootstrap = function(data) {
@@ -47,13 +62,23 @@ my_bootstrap = function(data) {
     return(means);
 }
 
-# hist
-# qqplot
+png(file = "plots/earnings_hist.png");
+hist(earnings);
+dev.off();
+
+png(file = "plots/earnings_qq.png");
+qqnorm(earnings, main = "Q-Q Plot of earnings");
+qqline(earnings);
+dev.off();
 
 earnings.bootstrap = my_bootstrap(earnings);
 earnings.bootstrap.mean = mean(earnings.bootstrap);
 
+cat("Earnings mean (bootstrap): ", earnings.bootstrap.mean, "\n");
+
+print("Confidence interval for 0.9:");
 quantile(earnings.bootstrap, probs = c(0.05, 0.95));
+print("Confidence interval for 0.95:");
 quantile(earnings.bootstrap, probs = c(0.025, 0.975));
 
 # 2
@@ -74,9 +99,56 @@ adv_bootstrap = function(data, f) {
 }
 
 # CASE 1
+
+png(file = "plots/shrimps_qq.png");
+qqnorm(shrimp, main = "Q-Q Plot of shrimps in cocktails");
+qqline(shrimp);
+dev.off();
+
 shrimps.bootstrap.mean = adv_bootstrap(shrimp, mean);
 shrimps.bootstrap.sd = adv_bootstrap(shrimp, sd);
 shrimps.bootstrap.var = shrimps.bootstrap.sd ^ 2;
+
+print("Confidence intervals for 0.95 for shrimps:");
+print("Mean:");
 quantile(shrimps.bootstrap.mean, probs = c(0.025, 0.975));
+print("Standard deviation:");
 quantile(shrimps.bootstrap.sd, probs = c(0.025, 0.975));
+print("Standard error:");
 quantile(shrimps.bootstrap.var, probs = c(0.025, 0.975));
+
+# CASE 2
+png(file = "plots/sitkasize_qq.png");
+qqnorm(Sitka89$size, main = "Q-Q Plot of sitka trees size");
+qqline(Sitka89$size);
+dev.off();
+
+sitkasize.bootstrap.mean = adv_bootstrap(Sitka89$size, mean);
+sitkasize.bootstrap.sd = adv_bootstrap(Sitka89$size, sd);
+sitkasize.bootstrap.var = sitkasize.bootstrap.sd ^ 2;
+
+print("Confidence intervals for 0.95 for sitka size:");
+print("Mean:");
+quantile(sitkasize.bootstrap.mean, probs = c(0.025, 0.975));
+print("Standard deviation:");
+quantile(sitkasize.bootstrap.sd, probs = c(0.025, 0.975));
+print("Standard error:");
+quantile(sitkasize.bootstrap.var, probs = c(0.025, 0.975));
+
+# CASE 3
+png(file = "plots/quineabsent_qq.png");
+qqnorm(quine$Days, main = "Q-Q Plot of days absent from school");
+qqline(quine$Days);
+dev.off();
+
+quineabsent.bootstrap.mean = adv_bootstrap(quine$Days, mean);
+quineabsent.bootstrap.sd = adv_bootstrap(quine$Days, sd);
+quineabsent.bootstrap.var = quineabsent.bootstrap.sd ^ 2;
+
+print("Confidence intervals for 0.95 for absent days:");
+print("Mean:");
+quantile(quineabsent.bootstrap.mean, probs = c(0.025, 0.975));
+print("Standard deviation:");
+quantile(quineabsent.bootstrap.sd, probs = c(0.025, 0.975));
+print("Standard error:");
+quantile(quineabsent.bootstrap.var, probs = c(0.025, 0.975));
